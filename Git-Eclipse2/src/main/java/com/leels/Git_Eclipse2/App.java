57d0c107291cfa66187764819d85540910038a23
Leelamaya <leelamayajena1@gmail.com>
2019-04-09 12:21:40 +0530

    1st commit

---
+++ Git-Eclipse2/src/main/java/com/leels/Git_Eclipse2/App.java
@@ -0,0 +1,13 @@
+package com.leels.Git_Eclipse2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
